3861bf7987ac5dd364efd9bb0969b03c3608e420
majoalba20 <mjalba458@gmail.com>
2022-03-16 15:12:06 -0500

    Arreglando toggle botones categorias

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Buttons = ({text , icon, filtroCategory,cat}) => {
+const Buttons = ({ text, icon, filtroCategory, cat, setButtonActive, buttonActive}) => {
 
-    const [active, setActive] = useState(false)
-
-    const toggle = ()=>{
-        setActive(!active)
-    }
-    
     return (
-        <div onClick={toggle}>
-            <button onClick={()=>filtroCategory(cat)} className={(active ? 'bg-botonActive flex items-center shadow-xl rounded-lg p-2' : 'bg-boton flex items-center shadow-xl rounded-lg p-2')}>
+        <div onClick={()=>setButtonActive(text)}>
+            <button onClick={() => filtroCategory(cat)} className={(buttonActive === text ? 'bg-botonActive flex items-center shadow-xl rounded-lg p-2' : 'bg-boton flex items-center shadow-xl rounded-lg p-2')}>
                 <img className='w-5 mr-2' src={icon} alt="" />
                 <p className='font-fredoka text-sm text-white'>{text}</p>
             </button>
